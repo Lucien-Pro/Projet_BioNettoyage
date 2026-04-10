87ffed418332478214d1c773cea6856f44b51d0d
Lucien-Pro <lucien.n@outlook.fr>
2026-04-10 11:22:36 +0200

    MAj

--- app/Http/Middleware/CheckPasswordChange.php
+++ app/Http/Middleware/CheckPasswordChange.php
@@ -15,7 +15,7 @@ class CheckPasswordChange
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->check() && auth()->user()->must_change_password && !$request->routeIs('password.force-change', 'logout')) {
+        if (auth()->check() && auth()->user()->must_change_password && !$request->routeIs('password.force-change', 'password.force-change.update', 'logout')) {
             return redirect()->route('password.force-change');
         }
 
