e254682ca3d51645b49188bb13521c768507a541
Lucien-Pro <lucien.n@outlook.fr>
2026-04-22 11:53:22 +0200

    MAJ : logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,9 @@
+<svg {{ $attributes->merge(['class' => 'w-16 h-16']) }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill="none">
+    <!-- Goutte d'eau -->
+    <path d="M50,10 C70,10 85,25 85,45 C85,65 50,90 50,90 C50,90 15,65 15,45 C15,25 30,10 50,10 Z"
+          stroke="#00a1e4" stroke-width="3" fill="none"/>
+
+    <!-- Feuille -->
+    <path d="M50,45 C55,35 65,40 60,50 C65,60 55,65 50,55 C45,65 35,60 40,50 C35,40 45,35 50,45 Z"
+          fill="#7cb342"/>
 </svg>
